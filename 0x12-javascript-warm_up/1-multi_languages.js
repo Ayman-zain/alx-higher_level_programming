2bdbe27da50a6550b7f050630941cbb3a7a30989
Ayman-zain <aymnzyn85@gmail.com>
2024-06-22 02:10:44 +0300

    1 again

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-let myVar = "C is fun";
-console.log(myVar);
-myVar = "Python is cool";
-console.log(myVar);
-myVar = "JavaScript is amazing";
-console.log(myVar);
+// It prints 3 lines
+
+console.log('C is fun\nPython is cool\nJavascript is amazing');
