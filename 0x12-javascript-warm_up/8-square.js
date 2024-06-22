bd9e65e6c623ddef47b27e48ff4b426c80d9dcf1
Ayman-zain <aymnzyn85@gmail.com>
2024-06-22 09:26:38 +0300

    8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { argv } = require('node:process');
+
+if (!isNaN(argv[2])) {
+  for (let i = 0; i < argv[2]; i++) {
+    for (let j = 0; j < argv[2]; j++) {
+      process.stdout.write('X');
+    }
+    console.log();
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
