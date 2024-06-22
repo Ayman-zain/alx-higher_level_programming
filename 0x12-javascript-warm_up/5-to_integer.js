bc422cf01e89c4cd3d09a03d450e14f8ce1727cf
Ayman-zain <aymnzyn85@gmail.com>
2024-06-22 08:28:11 +0300

    5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const { argv } = require('node:process');
 if (!isNaN(argv[2])) {
   console.log(`My number: ${argv[2]}`);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
